4a1056b77d057acd8271b2f541ee48bb66d62fe3
Daniel Hillenbrand <daniel.hillenbrand@codeworkx.de>
2012-07-23 16:38:51 +0200

    jellybeaned

--- p5110.mk
+++ p5110.mk
@@ -20,5 +20,10 @@ LOCAL_PATH := device/samsung/p5110
 
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+# Audio
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/tiny_hw.xml:system/etc/sound/espresso10wifi \
+	$(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
+
 # Use the non-open-source parts, if they're present
 $(call inherit-product, vendor/samsung/p51xx/p5110-vendor.mk)
