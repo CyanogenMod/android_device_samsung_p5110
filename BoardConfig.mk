cd0557aec380030bfa0edbf25002293c0d4eec33
codeworkx <codeworkx@cyanogenmod.com>
2012-11-06 19:09:53 +0100

    BoardConfig: allow flashing of p5110 builds on p5113
    Change-Id: I8ec40506e618fba1d56b244e31e15c16451dbcb0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,4 +24,4 @@ TARGET_KERNEL_SOURCE := kernel/samsung/espresso10
 TARGET_KERNEL_CONFIG := cyanogenmod_p5110_defconfig
 
 # assert
-TARGET_OTA_ASSERT_DEVICE := p5110,GT-P5110,espresso10wifi,espresso10wifixx
+TARGET_OTA_ASSERT_DEVICE := p5110,GT-P5110,p5113,GT-P5113,espresso10wifi,espresso10wifixx
