e1ec4712bca56e74cdb52644592653e1646c811b
codeworkx <codeworkx@cyanogenmod.org>
2012-12-18 01:14:23 +0100

    p5110: include camera wrapper
    Change-Id: I2351143b2b0baff6467d9ba45363a5d71b3c8bc2

--- Android.mk
+++ Android.mk
@@ -24,7 +24,7 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),p5110)
 
-COMMON_P51XX_DIRS := audio liblights power
+COMMON_P51XX_DIRS := audio camerawrapper liblights power
 include $(foreach common_dirs,$(COMMON_P51XX_DIRS),device/samsung/p5100/$(common_dirs)/Android.mk)
 
 endif
