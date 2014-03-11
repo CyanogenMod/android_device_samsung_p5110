49dff177febbbaa9cca2c1e46d468edc459e2575
Andreas B <skate4life@gmx.de>
2014-03-11 01:05:24 +0100

    p5110: Device makefile cleanup
    -Removes the redundant full_p5110
    -Standardizes the layout of omni_p5110
    -add readme
    -Updates the fingerprint to P5110XXDML1 (4.2.2 Stock)
    
    Change-Id: I14510efdf92d01517d3e7eddac92fa5315b259b2

--- omni_p5110.mk
+++ omni_p5110.mk
@@ -15,21 +15,25 @@
 # limitations under the License.
 #
 
-# Release name
-PRODUCT_RELEASE_NAME := p5110
-
-# Inherit some common Omni stuff.
+# Inherit common Omni configurations
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/samsung/p5110/full_p5110.mk)
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit device specific configurations
+$(call inherit-product, device/samsung/p5110/device.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := p5110
-PRODUCT_NAME := omni_p5110
-PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-P5110
+PRODUCT_BRAND := samsung
+PRODUCT_NAME := omni_p5110
+PRODUCT_DEVICE := p5110
 PRODUCT_MANUFACTURER := samsung
 
 #Set build fingerprint / ID / Prduct Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=espresso10wifixx TARGET_DEVICE=espresso10wifi BUILD_FINGERPRINT=samsung/espresso10wifixx/espresso10wifi:4.0.3/IML74K/P5110XWALD2:user/release-keys PRIVATE_BUILD_DESC="espresso10wifixx-user 4.0.3 IML74K P5110XWALD2 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=espresso10wifixx \
+    TARGET_DEVICE=espresso10wifi \
+    PRIVATE_BUILD_DESC="espresso10wifixx-user 4.2.2 JDQ39 P5110XXDML1 release-keys" \
+    BUILD_FINGERPRINT="samsung/espresso10wifixx/espresso10wifi:4.2.2/JDQ39/P5110XXDML1:user/release-keys"
