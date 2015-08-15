131912068873654aa6bfc8e040f4d3a318442fcb
Andreas Blaesius <skate4life@gmx.de>
2015-08-15 16:36:59 +0200

    P51XX: rework overlay
    Split CM and AOSP specific overlay.
    
    Change-Id: Idc1d922b9499c3b22ec4a59974d856b7d5a85eaa

--- cm.mk
+++ cm.mk
@@ -27,6 +27,9 @@ $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 # Inherit device configuration
 $(call inherit-product, device/samsung/p5110/full_p5110.mk)
 
+# CyanogenMod specific overlay
+DEVICE_PACKAGE_OVERLAYS += device/samsung/p5100/overlay/cm-common
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := p5110
 PRODUCT_NAME := cm_p5110
