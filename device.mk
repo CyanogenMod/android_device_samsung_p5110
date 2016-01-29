483837de6c1d907496612f92e84ca2c707c1f991
Ziyan <jaraidaniel@gmail.com>
2016-01-29 12:10:24 +0100

    audio: select correct audio config at runtime (5/5)
    Change-Id: Id13bdbd359e0e5af8ee9c1ce74c8cc549ec9f587

--- device.mk
+++ device.mk
@@ -23,7 +23,6 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay/aosp
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/tiny_hw.xml:system/etc/sound/espresso10wifi \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
 
 # IR packages
