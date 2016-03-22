35eb39fff07f662e2eeae5b38420c8a40217668c
Andreas Blaesius <skate4life@gmx.de>
2016-03-22 04:43:06 -0700

    Update TARGET_OTA_ASSERT_DEVICE
    Use espresso-common recovery instead espresso to avoid conflicts with HTC's espresso (myTouch 3G Slide T-Mobile)
    
    Change-Id: I0e61e4b21e6e7f31b6ba1914f1cd371b096f8781

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,7 +24,7 @@ TARGET_KERNEL_SOURCE := kernel/samsung/espresso10
 TARGET_KERNEL_CONFIG := cyanogenmod_p5110_defconfig
 
 # assert
-TARGET_OTA_ASSERT_DEVICE := espresso,p5110,GT-P5110,p5113,GT-P5113,espresso10wifi,espresso10wifixx
+TARGET_OTA_ASSERT_DEVICE := espresso-common,p5110,GT-P5110,p5113,GT-P5113,espresso10wifi,espresso10wifixx
 
 # Use the non-open-source parts, if they're present
 -include vendor/samsung/p51xx/BoardConfigVendor.mk
